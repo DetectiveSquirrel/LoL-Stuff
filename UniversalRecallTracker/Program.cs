172cce29322ac765d37db04efa8102b3b56d269a
Detective Squirrel <ratman7539@windowslive.com>
2016-09-06 16:41:56 +1200

    Update Program.cs

--- UniversalRecallTracker/Program.cs
+++ UniversalRecallTracker/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using LeagueSharp;
@@ -227,15 +227,17 @@ namespace UniversalRecallTracker
                     case Packet.S2C.Teleport.Status.Start:
                         _begin = Game.ClockTime;
                         _duration = decoded.Duration;
+                        Program.Instance().Notify(_hero.ChampionName + " has started recalling with " + (int) _hero.HealthPercent + " HP%");
                         _active = true;
                         break;
                     case Packet.S2C.Teleport.Status.Finish:
                         int colorIndex = (int) ((_hero.HealthPercent / 100) * 255);
                         string color = (255 - colorIndex).ToString("X2") + colorIndex.ToString("X2") + "00";
-                        Program.Instance().Notify(_hero.ChampionName + " has recalled with <font color='#" + color + "'>" + (int) _hero.HealthPercent + "&#37; HP</font>");
+                        Program.Instance().Notify(_hero.ChampionName + " has recalled with " + (int) _hero.HealthPercent + " HP%");
                         _active = false;
                         break;
                     case Packet.S2C.Teleport.Status.Abort:
+                        Program.Instance().Notify(_hero.ChampionName + " has aborted recalling with " + (int) _hero.HealthPercent + " HP%");
                         _active = false;
                         break;
                     case Packet.S2C.Teleport.Status.Unknown:
